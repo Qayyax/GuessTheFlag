b2003bbe591ffca86f96baf0d6e6dae79a4d0624
Qayyax <106759130+Qayyax@users.noreply.github.com>
2025-08-08 01:17:53 -0400

    added images, working with flags

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -9,20 +9,22 @@ import SwiftUI
 
 struct ContentView: View {
   @State private var showingAlert = false
-  
-    var body: some View {
-      VStack{
-        Button("Show Alert") {
-          showingAlert = true
-        }
-        .alert("Important message", isPresented: $showingAlert) {
-          Button("Delete", role: .destructive) {}
-          Button("Cancel", role: .cancel) {}
-        }
+  var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Spain", "UK", "Ukraine", "US"]
+  var correctAnswer  = Int.random(in: 0...2)
+  var body: some View {
+    VStack(spacing: 30) {
+      VStack {
+        Text("Tap the flag of")
+        Text(countries[correctAnswer])
       }
+      
+      ForEach(0..<3) { number in
+        Button {
+          // flag was tapped
+        } label: {
+          Image(countries[number])
+        }}
     }
-  func executeDelete() {
-    print("Now deleting")
   }
 }
 
