498b7761ec0027f4a1939999f2241f226ee3a4d4
Qayyax <106759130+Qayyax@users.noreply.github.com>
2025-08-08 01:34:14 -0400

    flags with zstack to add bg

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -12,18 +12,21 @@ struct ContentView: View {
   var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Spain", "UK", "Ukraine", "US"]
   var correctAnswer  = Int.random(in: 0...2)
   var body: some View {
-    VStack(spacing: 30) {
-      VStack {
-        Text("Tap the flag of")
-        Text(countries[correctAnswer])
+    ZStack {
+      Color.blue.ignoresSafeArea(edges: .all)
+      VStack(spacing: 30) {
+        VStack {
+          Text("Tap the flag of")
+          Text(countries[correctAnswer])
+        }
+        
+        ForEach(0..<3) { number in
+          Button {
+            // flag was tapped
+          } label: {
+            Image(countries[number])
+          }}
       }
-      
-      ForEach(0..<3) { number in
-        Button {
-          // flag was tapped
-        } label: {
-          Image(countries[number])
-        }}
     }
   }
 }
