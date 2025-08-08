c741f3fd7c19c0d3becb9f99c5521ade03ac1f32
Qayyax <106759130+Qayyax@users.noreply.github.com>
2025-08-08 09:03:27 -0400

    methods created

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -8,16 +8,22 @@
 import SwiftUI
 
 struct ContentView: View {
-  @State private var showingAlert = false
-  var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Spain", "UK", "Ukraine", "US"]
-  var correctAnswer  = Int.random(in: 0...2)
+  @State private var showingScore = false
+  @State private var scoreTitle = ""
+  
+  @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Spain", "UK", "Ukraine", "US"].shuffled()
+  @State private var correctAnswer  = Int.random(in: 0...2)
+  
   var body: some View {
     ZStack {
       Color.blue.ignoresSafeArea(edges: .all)
       VStack(spacing: 30) {
         VStack {
           Text("Tap the flag of")
+            .foregroundStyle(.white)
+          
           Text(countries[correctAnswer])
+            .foregroundStyle(.white)
         }
         
         ForEach(0..<3) { number in
@@ -28,6 +34,20 @@ struct ContentView: View {
           }}
       }
     }
+  }
+  // after the body
+  func flagTapped(_ number: Int) {
+    if number == correctAnswer {
+      scoreTitle = "Correct"
+    } else {
+      scoreTitle = "Wrong"
+    }
+    showingScore = true
+  }
+  
+  func askQuestion() {
+    countries.shuffle()
+    correctAnswer = Int.random(in: 0...2)
   }
 }
 
