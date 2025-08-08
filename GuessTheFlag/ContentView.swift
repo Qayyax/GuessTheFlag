03ad3e3b900305acb4fab76a44050a65390b0261
Qayyax <106759130+Qayyax@users.noreply.github.com>
2025-08-08 11:14:39 -0400

    finished this project

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -11,6 +11,9 @@ struct ContentView: View {
   @State private var showingScore = false
   @State private var scoreTitle = ""
   @State private var score = 0
+  @State private var questionCount = 0
+  @State private var showingFinalAlert = false
+  let maxQuestionCount = 8
   
   @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Spain", "UK", "Ukraine", "US"].shuffled()
   @State private var correctAnswer  = Int.random(in: 0...2)
@@ -75,21 +78,36 @@ struct ContentView: View {
         Text("Your score is \(score)")
       }
     }
+    
+    .alert("Game ended", isPresented: $showingFinalAlert) {
+      Button("Restart", action:askQuestion)
+    }message: {
+      Text("You have answered \(score) questions out of \(maxQuestionCount) correctly.")
+    }
   }
   // after the body
   func flagTapped(_ number: Int) {
+    questionCount += 1
     if number == correctAnswer {
       scoreTitle = "Correct"
       score += 1
     } else {
       scoreTitle = "Wrong"
     }
-    showingScore = true
+    if questionCount == maxQuestionCount {
+      showingScore = false
+      showingFinalAlert = true
+    } else {
+      showingScore = true
+    }
   }
   
   func askQuestion() {
     countries.shuffle()
     correctAnswer = Int.random(in: 0...2)
+    if questionCount == maxQuestionCount {
+      score = 0
+    }
   }
 }
 
